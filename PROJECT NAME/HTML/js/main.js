27f614f65fc50123a49e4a5b1c1400e705dc5e0b
666hesoyam <56505654+666hesoyam@users.noreply.github.com>
2019-12-17 19:20:53 +0200

    upd

--- PROJECT NAME/HTML/js/main.js
+++ PROJECT NAME/HTML/js/main.js
@@ -1,27 +1,7 @@
-var main = function () {
+// var main = function () {
     
-    // handle on page scroll
-    var handleHeaderOnScroll = function() {
-        if ($(window).scrollTop() > 60) {
-            $("body").addClass("page-on-scroll");
-        } else {
-            $("body").removeClass("page-on-scroll");
-        }
-    }
+// // code
 
-    return {
-        init: function () {
-            // initial setup for fixed header
-            handleHeaderOnScroll();
-
-            // handle minimized header on page scroll
-            $(window).scroll(function() {
-                handleHeaderOnScroll();
-            });
-        },
-    };
-}();
-
-$(document).ready(function() {
-    main.init();
-});
+// $(document).ready(function() {
+//     main.init();
+// });
